f4e4bf67581d494f58a18a073e1704ddf9f9c974
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-01 16:11:18 +0100

    fichier de log db

---
+++ website/loginDatabase/login.php
@@ -0,0 +1,14 @@
+ <?php
+ $host = "localhost"; /* L'adresse de l'hôte MySQL */
+ $login = "id8552472_smartplante"; /* Votre numéro d'utilisateur MySQL*/
+ $password = "r0vskuk8HH5GRbFt"; /* Votre mot de passe */
+ $base = "id8552472_spVariables"; /* Le nom de votre base */
+
+ function connexion()
+ {
+     global $host, $login, $password, $base;
+	 $db = mysqli_connect($host, $login, $password, $base);
+	 mysqli_select_db($db,$base);
+     return $db;
+ }
+?>
