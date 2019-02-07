46bae7b006ad1992e3a91da21d28101e94533bbc
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-07 17:17:23 +0100

    version vidéo 07/02

--- arduino/master/smartplanteFull.ino
+++ arduino/master/smartplanteFull.ino
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Grove_I2C_Motor_Driver.h>
@@ -15,9 +14,6 @@ Ultrasonic ultrasonic(8);
 int pir1 = 3; //Capteur d'arret
 int pir2 = 2; //Capteur de mise en marche
 int ae = 0;
-
-void requete();
-void connexion();
 
 void setup() {
 Serial.begin(9600);
@@ -52,7 +48,7 @@ int ProgMoteur(int CaptMarche, int CaptStop) {
     Serial.println("Moteur stade Arret");
   }
 
-  Serial.println("La distance capteur obstacle est de  : ");
+  Serial.print("La distance capteur obstacle est de : ");
   long Distance = ultrasonic.MeasureInCentimeters(); // Envoie de la distance en centimétre entre le capteur Ultrason et le Sol
   Serial.print(Distance);
   Serial.println(" cm");
@@ -71,17 +67,18 @@ if (Ethernet.begin(mac) == 0) { //detecter en cas de probleme d'ip (DHCP = IP dy
 
 void requete(int hauteur) {
 if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
-  Serial.println("connection OK");
-  client.print("GET http://thibaultpech.000webhostapp.com/acq2donee.php?DATA="); //url envoyé par le client arduino | viens du vieux tp 2018
+  Serial.println("*Connectée");
+  client.print("GET http://projetsmartplante.000webhostapp.com/sendVarTest/varWriter.php?DATA="); //url envoyé par le client arduino | viens du vieux tp 2018
   client.print(hauteur); //var 1
   client.print("&DATA2=");
   client.print("pasDeValeur"); //var 2 | add 2nd valeur here
   client.println(" HTTP/1.1"); //NE FAIT PAS PARTIE DE L'URL
   client.println();
-  Serial.println("Valeur Hauteur envoyée");
+  Serial.println("**Valeurs envoyées");
   client.stop();
   }
+
 else {
-  Serial.println("connection failed");
+  Serial.println("**Connexion impossible");
   }
 }
