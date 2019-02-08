8cd007d423a8b422ae4338191da96dce4878e624
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-08 22:12:36 +0100

    maj folder maj reception des var ds file - 4var now from :https://docs.google.com/spreadsheets/d/1y4YCCO0IK9F7UMrY4Qxlgv7oJCIh7HcX2ZeLTCIK7xU - reception des données en ligne ds le debug file

---
+++ website/Test/varWriter.php
@@ -0,0 +1,20 @@
+<?php
+$hauteur = $_GET['hauteur']; //ben (val num sans virgule)
+$ventilateur = $_GET['ventilateur']; //romain (0/1?)
+$humidité = $_GET['humidité']; //flo
+$QO = $_GET['QO']; //flo
+echo nl2br("hauteur=$hauteur \n ventilateur=$ventilateur \n humidité=$humidité \n QO=$QO");
+$file=fopen("debugVar.txt","w");
+fwrite($file,"hauteur=");
+fwrite($file, $hauteur);
+fwrite($file, "\r\n");
+fwrite($file,"ventilateur=");
+fwrite($file, $ventilateur);
+fwrite($file, "\r\n");
+fwrite($file,"humidité=");
+fwrite($file, $humidité);
+fwrite($file, "\r\n");
+fwrite($file,"QO=");
+fwrite($file, $QO);
+fclose($file);
+?>
