74cfaa26bff219ec80f2ae59dd850f22991f79be
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-09 11:01:38 +0100

    création variables ds database (a tester)

---
+++ website/Database/installation.php
@@ -0,0 +1,16 @@
+<?php
+ include("login.php");
+
+ $db=connexion();
+////////////////*création de la table dans la base de données*////////////////////////////
+
+ $sql= "CREATE TABLE IF NOT EXISTS `id8552472_Test` (";
+ $sql .= "`hauteur` SMALLINT NOT NULL,";
+ $sql .= "`ventilateur` TINYINT NOT NULL,";
+ $sql .= "`humidité` SMALLINT(5,5) NOT NULL,";
+ $sql .= "`QO` SMALLINT NOT NULL,";
+ $sql .= "KEY `id` (`id`)";
+ $sql .= ") ENGINE=MyISAM;";
+ echo ($sql) ;
+ mysqli_query($db,$sql) or die(mysqli_connect_error());
+?>
