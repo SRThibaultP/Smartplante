aab3c7fe94061fd6b65ac05b8df4e84f2bf5760e
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-10 00:31:35 +0100

    homogénité du fichier en lien avec les autres programmes

---
+++ arduino/florian/programme_dose_humide.ino
@@ -0,0 +1,26 @@
+int sensorPin = A0;                  //initialisation variable SensorPin (capteur humidité) sur entree analogique AO
+int sensorValue = 0;                 //initialisation variable du capteur sur O
+
+void setup() {
+Serial.begin(9600);                  //débit du moniteur de 9 600 bits par seconde.
+}
+
+void loop() {
+sensorValue = analogRead(sensorPin); //lecture de la valeur analogique par le capteur et stockage dans variable sensorValue
+Serial.print("humidite = " );        //écriture sur le moniteur série de humidité
+Serial.println(sensorValue);         //écriture sur le moniteur série de la valeur numerique de SensorValue
+delay(1000);                         //attente durant 1 Seconde
+
+if (sensorValue<307) {               // Si la valeur numérique est inférieur à 307
+  Serial.print("humidite = terrain sec" );        //écriture sur le moniteur série de humidité = terrain sec
+}
+
+else
+  if (sensorValue>700) {             // Si la valeur numérique est supérieur à 700
+    Serial.print("humidite = terrain très humide" );     //écriture sur le moniteur série de humidité = terrain très humide
+  }
+  else
+    if (sensorValue<700) {           // Si la valeur numérique est inférieur à 700
+      Serial.print("humidite = terrain humide" );        //écriture sur le moniteur série de humidité = terrain humide
+    }
+}
