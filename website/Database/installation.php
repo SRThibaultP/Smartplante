104625fbd8ec5933870790e90069b6ea5c5a9118
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-10 20:12:38 +0100

    Database crée

--- website/Database/installation.php
+++ website/Database/installation.php
@@ -4,11 +4,14 @@
  $db=connexion();
 ////////////////*création de la table dans la base de données*////////////////////////////
 
- $sql= "CREATE TABLE IF NOT EXISTS `id8552472_Test` (";
- $sql .= "`hauteur` SMALLINT NOT NULL,";
- $sql .= "`ventilateur` TINYINT NOT NULL,";
- $sql .= "`humidité` SMALLINT(5,5) NOT NULL,";
- $sql .= "`QO` SMALLINT NOT NULL,";
+ $sql= "CREATE TABLE IF NOT EXISTS `id8552472_dbvariables` (";
+ $sql .= "`id` int(11) NOT NULL auto_increment,";
+ $sql .= "`hauteur` int NOT NULL,";
+ $sql .= "`ventilateur` int NOT NULL,";
+ $sql .= "`humidité` int NOT NULL,";
+ $sql .= "`QO` int NOT NULL,";
+ $sql .= "PRIMARY KEY  (`id`),";
+ $sql .= "UNIQUE KEY `id_2` (`id`),";
  $sql .= "KEY `id` (`id`)";
  $sql .= ") ENGINE=MyISAM;";
  echo ($sql) ;
