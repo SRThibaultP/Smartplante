644e87f4e322909c12924ebeba25f5fcaa7e4eaf
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-19 22:52:26 +0100

    Envoie de donnée vers la database

--- website/Database/varadd.php
+++ website/Database/varadd.php
@@ -3,8 +3,8 @@
  if ($_POST['hauteur'] != '' AND $_POST['ventilateur'] != '' AND $_POST['humidité'] != '' AND $_POST['QO'] != '')
   {
     $db=connexion();
-	  $sql = "INSERT INTO id8552472_dbvariables(id,hauteur,ventilateur,humidité,QO) ";
- 	  $sql .= "VALUES('','".$_POST['hauteur']."','".$_POST['ventilateur']."','".$_POST['humidité']."','".$_POST['QO']."')";
+	  $sql = "INSERT INTO id8552472_dbvariables (hauteur,ventilateur,humidité,QO)
+    VALUES ('$_POST[hauteur]', '$_POST[ventilateur]', '$_POST[humidité]', '$_POST[QO]')";
 	  mysqli_query($db,$sql) or die(mysqli_error($db));
 	}
 
