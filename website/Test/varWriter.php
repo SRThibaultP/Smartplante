0821e60ef719233ee560da0903ba0789c1a0eb49
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-02-27 21:32:45 +0100

    Possible fix au probleme d'envoie de requete (calqué sur "acq2donee.php" du tp web)

--- website/Test/varWriter.php
+++ website/Test/varWriter.php
@@ -1,19 +1,16 @@
 <?php
-$hauteur = $_GET['hauteur']; //ben (val num sans virgule)
-$ventilateur = $_GET['ventilateur']; //romain (valeur en 0/1?)
-$humidité = $_GET['humidité']; //flo
-$QO = $_GET['QO']; //flo
-echo nl2br("hauteur=$hauteur \n ventilateur=$ventilateur \n humidité=$humidité \n QO=$QO");
+$hauteur = $_GET['hauteur'];
+$ventilateur = $_GET['ventilateur'];
+$humidité = $_GET['humidité'];
+$QO = $_GET['QO'];
+echo "hauteur=".$hauteur." ventilateur=".$ventilateur." humidité=".$humidité." QO=".$QO."";
 $file=fopen("debugVar.txt","w");
 fwrite($file,"hauteur=");
 fwrite($file, $hauteur);
-fwrite($file, "\r\n");
 fwrite($file,"ventilateur=");
 fwrite($file, $ventilateur);
-fwrite($file, "\r\n");
 fwrite($file,"humidité=");
 fwrite($file, $humidité);
-fwrite($file, "\r\n");
 fwrite($file,"QO=");
 fwrite($file, $QO);
 fclose($file);
