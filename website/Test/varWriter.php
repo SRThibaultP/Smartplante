fa8495eda4e973c6260a27a41cd9cef17c873cbc
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-01 15:36:44 +0100

    recodage du code reception de données

--- website/Test/varWriter.php
+++ website/Test/varWriter.php
@@ -1,17 +1,17 @@
-<?php
-$hauteur = $_GET['hauteur'];
-$ventilateur = $_GET['ventilateur'];
-$humidité = $_GET['humidité'];
-$QO = $_GET['QO'];
-echo "hauteur=".$hauteur." ventilateur=".$ventilateur." humidité=".$humidité." QO=".$QO."";
-$file=fopen("debugVar.txt","w");
-fwrite($file,"hauteur=");
-fwrite($file, $hauteur);
-fwrite($file,"ventilateur=");
-fwrite($file, $ventilateur);
-fwrite($file,"humidité=");
-fwrite($file, $humidité);
-fwrite($file,"QO=");
-fwrite($file, $QO);
-fclose($file);
+<?php
+$hauteur = $_GET['hauteur'];
+$ventilateur = $_GET['ventilateur'];
+$humidite = $_GET['humidite'];
+$eau = $_GET['eau'];
+echo "Data ".$hauteur." et ".$humidite." et ".$ventilateur." et ".$eau."bien acquise sur le serveur ";
+$file=fopen("bank.txt","w");
+fwrite($file,"hauteur:");
+fwrite($file, $hauteur);
+fwrite($file, " ventilateur:");
+fwrite($file, $ventilateur);
+fwrite($file, " humidite:");
+fwrite($file, $humidite);
+fwrite($file, " eau:");
+fwrite($file, $eau);
+fclose($file);
 ?>
