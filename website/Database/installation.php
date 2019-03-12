cd24aa2049d242dbd47d2651d35101e40d8167e5
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-12 12:12:50 +0100

    fix send var to database

--- website/Database/installation.php
+++ website/Database/installation.php
@@ -8,8 +8,8 @@
  $sql .= "`id` int(11) NOT NULL auto_increment,";
  $sql .= "`hauteur` decimal(11,3) NOT NULL,";
  $sql .= "`ventilateur` int(11) NOT NULL,";
- $sql .= "`humidité` decimal(11,3) NOT NULL,";
- $sql .= "`QO` decimal(11,3) NOT NULL,";
+ $sql .= "`humidite` decimal(11,3) NOT NULL,";
+ $sql .= "`eau` decimal(11,3) NOT NULL,";
  $sql .= "PRIMARY KEY  (`id`),";
  $sql .= "UNIQUE KEY `id_2` (`id`),";
  $sql .= "KEY `id` (`id`)";
