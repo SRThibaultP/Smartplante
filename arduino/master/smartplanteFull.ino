5e0c5c93b1e6d7260983d4c5aa6c801677817429
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-12 12:49:02 +0100

    modif commentaires

--- arduino/master/smartplanteFull.ino
+++ arduino/master/smartplanteFull.ino
@@ -55,7 +55,7 @@ int ProgMoteur(int CaptMarche, int CaptStop) {
 }
 
 void connexion () {
-if (Ethernet.begin(mac) == 0) { //detecter en cas de probleme d'ip (DHCP = IP dynamique)
+if (Ethernet.begin(mac) == 0) { //detecter pb d'ip (DHCP = IP dynamique)
     Serial.println("Failed to configure Ethernet using DHCP");
     for(;;);
     delay(1000);
@@ -66,15 +66,15 @@ if (Ethernet.begin(mac) == 0) { //detecter en cas de probleme d'ip (DHCP = IP dy
 void requete(int hauteur) {
 if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
   Serial.println("*Connectée");
-  client.print("GET http://projetsmartplante.000webhostapp.com/Database/varaddauto.php?hauteur="); //url envoyé par le client arduino | viens du vieux tp 2018
+  client.print("GET http://projetsmartplante.000webhostapp.com/Database/varaddauto.php?hauteur="); //url send arduino
   client.print(hauteur); //var 1
   client.print("&ventilateur=");
-  client.print("0"); //var 2 | add 2nd valeur here
+  client.print("0"); //var 2
   client.print("&humidite=");
-  client.print("99.99"); //var 2 | add 2nd valeur here
+  client.print("99.99"); //var 3
   client.print("&eau=");
-  client.print("999.999"); //var 2 | add 2nd valeur here
-  client.println(" HTTP/1.1"); //NE FAIT PAS PARTIE DE L'URL
+  client.print("999.999"); //var 4
+  client.println(" HTTP/1.1"); //NOT IN URL
   client.println();
   Serial.println("**Valeurs envoyées");
   client.stop();
