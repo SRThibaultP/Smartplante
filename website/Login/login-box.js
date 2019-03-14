9cad305c5bd7fa424e2f69e64f46e716c9b39f42
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-14 20:43:48 +0100

    Separation javascript & html

---
+++ website/Login/login-box.js
@@ -0,0 +1,6 @@
+$(".come-login-btn").on("click",function(){
+  $(".login-box").toggleClass("showed");
+});
+$(".hide-login-btn").on("click",function(){
+  $(".login-box").toggleClass("showed");
+});
