1b2b3e5b82aa0756d7b652c6b7dd64852ab88e99
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-16 22:50:25 +0100

    FIX ROOT DIRECTORY ```php include($_SERVER['DOCUMENT_ROOT'].'/Database/login.php'); ```

--- website/Serre/Rabelais/lastVar.php
+++ website/Serre/Rabelais/lastVar.php
@@ -1,6 +1,6 @@
 <?php
 $sql = "SELECT * FROM id8552472_dbvariables WHERE id = (SELECT MAX(id) FROM id8552472_dbvariables)";
-include("../Database/login.php");
+include($_SERVER['DOCUMENT_ROOT'].'/Database/login.php');
 $db=connexion();
 $req = mysqli_query($db,$sql) or die(mysqli_error());
 while($data = mysqli_fetch_assoc($req))
