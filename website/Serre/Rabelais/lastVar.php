aebe069051dfb6e12fd9abd6b18786521e627d90
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-18 21:59:52 +0100

    AFFICHAGE DES VALEURS DE LA BASE DE DONNEE VERS PAGE WEB

--- website/Serre/Rabelais/lastVar.php
+++ website/Serre/Rabelais/lastVar.php
@@ -3,8 +3,7 @@ $sql = "SELECT * FROM id8552472_dbvariables WHERE id = (SELECT MAX(id) FROM id85
 include($_SERVER['DOCUMENT_ROOT'].'/Database/login.php');
 $db=connexion();
 $req = mysqli_query($db,$sql) or die(mysqli_error());
-while($data = mysqli_fetch_assoc($req))
-{
+while($data = mysqli_fetch_assoc($req)){
   $hauteur = $data["hauteur"];
   $ventilateur = $data["ventilateur"];
   $humidite = $data["humidite"];
---
+++ website/Serre/Rabelais/serreRabelais.php
@@ -0,0 +1,36 @@
+<?php
+$sql = "SELECT * FROM id8552472_dbvariables WHERE id = (SELECT MAX(id) FROM id8552472_dbvariables)";
+include($_SERVER['DOCUMENT_ROOT'].'/Database/login.php');
+$db=connexion();
+$req = mysqli_query($db,$sql) or die(mysqli_error());
+while($data = mysqli_fetch_assoc($req)){
+  $hauteur = $data["hauteur"];
+  $ventilateur = $data["ventilateur"];
+  $humidite = $data["humidite"];
+  $eau = $data["eau"];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SerreRabelais - SmartPlante</title>
+    <link rel="icon" href="/Images/favicon-32x32.png" type="image/png">
+    <link rel="stylesheet" href="../serre.css">
+  </head>
+  <body>
+    <div class="hauteur">
+      <?php echo "$hauteur"; ?>
+    </div>
+    <div class="ventilateur">
+      <?php echo "$ventilateur"; ?>
+    </div>
+    <div class="humidite">
+      <?php echo "$humidite"; ?>
+    </div>
+    <div class="eau">
+      <?php echo "$eau"; ?>
+    </div>
+  </body>
+</html>
