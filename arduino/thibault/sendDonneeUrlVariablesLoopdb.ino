c7d441e455a60cdc927f9b2d7a43a7c6434d866a
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-20 17:33:27 +0100

    maj pour revue projet ang

--- arduino/thibault/sendDonneeUrlVariablesLoopdb.ino
+++ arduino/thibault/sendDonneeUrlVariablesLoopdb.ino
@@ -2,13 +2,7 @@
 #include <Ethernet.h>
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0xCD, 0xE9}; //adresse mac shield ethernet
-IPAddress server(178,32,28,116); //adresse ip arduino
 EthernetClient client;
-
-//A SUPPRIMER LORS DU CODE FINAL
-float ae,humidite,QO = 48.35;
-int ventilateur = 1; //db limit
-//A SUPPRIMER LORS DU CODE FINAL
 
 void setup() {
 Serial.begin(9600);  //Initialisation Moniteur Serie
@@ -16,7 +10,7 @@ Serial.begin(9600);  //Initialisation Moniteur Serie
 
 void loop() {
 connexion();
-requete(ae,humidite,QO,ventilateur);
+requete(ae);
 }
 
 void connexion () {
@@ -28,27 +22,23 @@ if (Ethernet.begin(mac) == 0) { //detecter en cas de probleme d'ip (DHCP = IP dy
   }
 }
 
-void requete(int hauteur, int ventilateur, int humidite, int QO) {
+void requete(int hauteur) {
 if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
   Serial.println("*Connectée");
-  client.print("GET http://projetsmartplante.000webhostapp.com/Database/varaddauto.php?hauteur="); //url envoyé par le client arduino
-  client.print("25.686");       //var 1
-  client.print("&ventilateur");
-  client.print("1"); //var 2 | add 2nd valeur here
-  client.print("&humidité"); //changer nom var? (start base sheets var)
-  client.print("15.12"); //var 3 | add 3nd valeur here | changer nom var? (start base sheets var)
-  client.print("&QO");
-  client.print("98"); //var 4 | add 4nd valeur here
-  client.println(" HTTP/1.1"); //NE FAIT PAS PARTIE DE L'URL
+  client.print("GET http://projetsmartplante.000webhostapp.com/Database/varaddauto.php?hauteur="); //url send arduino
+  client.print(hauteur); //var 1
+  client.print("&ventilateur=");
+  client.print("0"); //var 2
+  client.print("&humidite=");
+  client.print("99.99"); //var 3
+  client.print("&eau=");
+  client.print("999.999"); //var 4
+  client.println(" HTTP/1.1"); //NOT IN URL
   client.println();
-  //Serial.print(client.read());
-  //Serial.println("**Valeurs envoyées");
+  Serial.println("**Valeurs envoyées");
   client.stop();
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c); //ecrire dans le moniteur série les données du site web
-  }
-  }
+  delay(10000);
+}
 
 else {
   Serial.println("**Connexion impossible");
