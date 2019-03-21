060756374c981ee0072b9a900bbee5ad95239e29
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2019-03-21 15:46:15 +0100

    Fusion du code de Florian v1

--- arduino/master/smartplanteFull.ino
+++ arduino/master/smartplanteFull.ino
@@ -14,17 +14,25 @@ int pir1 = 3; //Capteur d'arret
 int pir2 = 2; //Capteur de mise en marche
 int ae = 0;
 
+int sensorPin = A1;                     //initialisation variable SensorPin (capteur humidité) sur entree analogique AO
+int sensorValue = 0;                    //initialisation variable du capteur sur O
+int vanPin = 8;
+
 void setup() {
 Serial.begin(9600);  //Initialisation Moniteur Serie
+
 Motor.begin(I2C_ADDRESS); //Localisation du moteur
 pinMode(pir1,INPUT);  //Mode Réception des données
 pinMode(pir2,INPUT);
+
+pinMode(vanPin, OUTPUT);
 }
 
 void loop(){
 connexion();
 ae = ProgMoteur(pir2, pir1);
-requete(ae);
+humidite();
+requete(ae,vanPin);
 }
 
 int ProgMoteur(int CaptMarche, int CaptStop) {
@@ -63,7 +71,7 @@ if (Ethernet.begin(mac) == 0) { //detecter pb d'ip (DHCP = IP dynamique)
     }
   }
 
-void requete(int hauteur) {
+void requete(int hauteur, int humidite) {
 if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
   Serial.println("*Connectée");
   client.print("GET http://projetsmartplante.000webhostapp.com/Database/varaddauto.php?hauteur="); //url send arduino
@@ -71,7 +79,7 @@ if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
   client.print("&ventilateur=");
   client.print("0"); //var 2
   client.print("&humidite=");
-  client.print("99.99"); //var 3
+  client.print(humidite); //var 3
   client.print("&temperature=");
   client.print("999.999"); //var 4
   client.println(" HTTP/1.1"); //NOT IN URL
@@ -83,5 +91,22 @@ if (client.connect("proxy-eple.in.ac-nantes.fr",3128)) { //proxy lycée
 
 else {
   Serial.println("**Connexion impossible");
+  }
+}
+
+void humidite (){
+sensorValue = analogRead(sensorPin); //lecture de la valeur analogique par le capteur et stockage dans variable sensorValue
+Serial.print("humidite = " );        //écriture sur le moniteur série de humidité
+Serial.println(sensorValue);         //écriture sur le moniteur série de la valeur numerique de SensorValue
+delay(1000);                         //attente durant 1 Seconde
+
+if (sensorValue<732) {               // Si la valeur numérique est inférieur à 732
+  digitalWrite(vanPin,HIGH);         // ouvre la vanne (commande le relais qui met en marche la pompe)
+  delay(1000);                       // pause 1 seconde
+  }
+
+if (sensorValue>732) {               // Si la valeur numérique est supérieur à 732
+  digitalWrite(vanPin,LOW);          // ferme la vanne (commande le relais qui met en marche la pompe)
+  delay(1000);                       // pause 1 seconde
   }
 }
