e9b8e1d9d7814f2c4ba0182036bb1288027af261
BenzOuill <codejena@gmail.com>
2019-04-24 09:03:06 +0200

    fg'

--- arduino/romain/full.ino
+++ arduino/romain/full.ino
@@ -12,6 +12,7 @@ int relay2 = 8;
 int ventilateur = 4;          // Relais connecté à la Pin 4
 int valInit = 0;
 int valStop = 1;
+float temp = 0;
 
 void setup()
 {
@@ -50,23 +51,47 @@ void setup()
 
 void loop()
 {
-
-    int a = analogRead(pinTempSensor);
-
-    float R = 1023.0/a-1.0;
-    R = R0*R;
-
-    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;    // Convertis en température
-
-    Serial.print("temperature = ");                           // Affiche "temperature ="
-    Serial.println(a);                                        // Affiche la température
-
-
-    if (temperature >= 30){                                   // Si température > 30°C
-      digitalWrite(ventilateur,HIGH);                         // Active le ventilateur
-      Serial.println("Trop chaud");                           // Affiche "Trop chaud"
-    }
-    if (temperature < 25) {                                   // Si température < 25°C
-      digitalWrite(ventilateur,LOW);                          // Désactive le ventilateur
-    }
+  temp = Chaleur();
  }
+
+float Chaleur(){
+
+
+
+ int a = analogRead(pinTempSensor);
+
+
+
+ float R = 1023.0/a-1.0;
+
+ R = R0*R;
+
+
+
+ float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;    // Convertis en température
+
+
+
+ Serial.print("temperature = ");                           // Affiche "temperature ="
+
+ Serial.println(a);                                        // Affiche la température
+
+
+
+
+
+ if (temperature >= 30){                                   // Si température > 30°C
+
+   digitalWrite(ventilateur,HIGH);                         // Active le ventilateur
+
+   Serial.println("Trop chaud");                           // Affiche "Trop chaud"
+
+ }
+
+ if (temperature < 25) {                                   // Si température < 25°C
+
+   digitalWrite(ventilateur,LOW);                          // Désactive le ventilateur
+
+ }
+return temperature;
+}
